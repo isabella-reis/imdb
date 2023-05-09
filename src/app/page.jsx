e1425a2eea1e6c688ea30d70cf0f5a7aa384fcb4
Isabella Reis <isabellanery15@hotmail.com>
2023-05-09 11:28:24 -0300

    Fetch data from TMDB API in home page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,3 +1,5 @@
+import Results from "@/components/Results";
+
 const API_KEY = process.env.API_KEY;
 
 export default async function Home({ searchParams }) {
@@ -6,16 +8,22 @@ export default async function Home({ searchParams }) {
   // genre is equal to searchParams.genre, if there's no genre we will use fetchTrending instead.
   const genre = searchParams.genre || "fetchTrending";
   const res = await fetch(
-    `https://api.themoviedb.org/3/movie/${
+    `https://api.themoviedb.org/3/${
       genre === "fetchTopRated" ? "movie/top_rated" : "trending/all/week"
     }?api_key=${API_KEY}&language=en-US&page=1`,
-    { next: { revalidate: 86400 } }
+    { next: { revalidate: 84000 } }
   );
 
   const data = await res.json();
   const results = data.results;
 
-  console.log(results);
+  if (!res.ok) {
+    throw new Error("Failed to fetch data.");
+  }
 
-  return <h1 className="text-red-700"></h1>;
+  return (
+    <div>
+      <Results results={results}></Results>
+    </div>
+  );
 }
